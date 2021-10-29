3dd3c30d60a4434722479d022acb9635ca74ac64
Madhav Anand <madhavanand402@gmail.com>
2021-10-29 11:41:47 +0530

    🚀 Fields Check Final

--- modules/requestAnalyser.js
+++ modules/requestAnalyser.js
@@ -1,18 +1,28 @@
 module.exports.allChecksPassed = function allChecksPassed(req,res){ 
+
+	let fieldCheck = isNumberOfFields(req, res);
+	if(fieldCheck){
+		console.log('All check passed : 👍');
+		return true;
+	}
+	else{
+		return false;
+	}
 	
-	// Number of fields
-	if(isNumberOfFields(req,res)){
+}
+
+//  Check if number of fields are nine
+function isNumberOfFields(req, res){
+	let count = Object.keys(req.body).length;
+	if(count == 9){
 		console.log('Number of fields : ✅');
+		return true;
 	} 
 	else {
 		console.log('Number of fields : ❌');
-		res.send('😕 Number of fields are incorrect or you have duplicate fields')
+		res.send('Number of fields are incorrect or you may have duplicate fields 😕');
+		return false;
 	}
-
 }
 
-//  Check if number of fields are nine, return boolean
-function isNumberOfFields(req){
-	let count = Object.keys(req.body).length;
-	return count == 9;
-}
+//

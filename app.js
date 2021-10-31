12ad9dcdc124bc15b551335feef93eb9263bd441
Madhav Anand <madhavanand402@gmail.com>
2021-10-31 23:05:55 +0530

    🚀 Final Change

--- app.js
+++ app.js
@@ -7,13 +7,12 @@ app.use(express.json());
 
 // Modules Import
 const {isEmpty, isCorrectField} = require('./modules/requestAnalyser.js');
-const {emptyFieldParser, fieldTypeParser, uniqueSoft, hardParser} = require('./modules/fieldsProcessor');
+const {emptyFieldParser, fieldTypeParser, uniqueSoft, serpUpdate, hardParser} = require('./modules/fieldsProcessor');
 
 // Route Creation
 const mainRouter = express.Router();
 app.use('/format-address', mainRouter);
-mainRouter
-.route('/')
-.post(isEmpty, isCorrectField, emptyFieldParser, fieldTypeParser, uniqueSoft, hardParser)
+mainRouter.route('/')
+.post(isEmpty, isCorrectField, emptyFieldParser, fieldTypeParser, uniqueSoft, serpUpdate, hardParser)
       
 

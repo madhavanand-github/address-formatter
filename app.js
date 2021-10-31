512f28e9bb7cea450e8a71e4a27396d68c46ac4a
Madhav Anand <madhavanand402@gmail.com>
2021-10-31 08:56:20 +0530

    🚀 Heroku Deployement

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
 // Boilerplate
 const express = require('express');
 const app = express();
-app.listen(3000, () => console.log('Server Listenning 👍'));
+let port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Server Listenning at ${port} 👍 `));
 app.use(express.json());
 
 // Modules Import
 const {isEmpty, isCorrectField} = require('./modules/requestAnalyser.js');
-const {emptyFieldParser, fieldTypeParser, uniqueSoft} = require('./modules/fieldsProcessor');
+const {emptyFieldParser, fieldTypeParser, uniqueSoft, hardParser} = require('./modules/fieldsProcessor');
 
 // Route Creation
 const mainRouter = express.Router();
 app.use('/format-address', mainRouter);
 mainRouter
 .route('/')
-.post(isEmpty, isCorrectField, emptyFieldParser, fieldTypeParser, uniqueSoft)
+.post(isEmpty, isCorrectField, emptyFieldParser, fieldTypeParser, uniqueSoft, hardParser)
       
 
